62834cb7660ecc6d0c2c7d497ca067afb774f7ce
sweida <weidaxyy@163.com>
2018-11-30 02:37:21 +0800

    s

---
+++ js/content.js
@@ -0,0 +1,8 @@
+// setInterval(function(){
+// 	if( Date.now() > new Date("2018-10-01 01:20:00")){
+// 		document.getElementsByClassName('hot-refresh')[0].click()
+// 	}
+// }, 1000);
+
+window.open('www.qq.com')
+window.open('http://sweida.me')
